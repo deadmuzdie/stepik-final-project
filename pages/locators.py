61869b9339a9766ea5d7d2d9f28c1fdf92044188
deadmuzdie <deadmuzdie@mail.ru>
2019-12-17 09:28:10 +0800

    Login Page initializing in body test

--- pages/locators.py
+++ pages/locators.py
@@ -5,11 +5,7 @@
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-#class LoginPage(BasePage):
 class LoginPageLocators(): 
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-#    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-#button name=login_submit form id=login_form
-#button name=registration_submit form id=register_form
 
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 ﻿from .base_page import BasePage
 from .locators import MainPageLocators
 from .locators import LoginPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): 
@@ -9,13 +10,10 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-    #    self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         url = self.browser.current_url
-#        if (url.find("login") != -1):
         assert url.find("login") != -1, "Not in login page here"
 
     def should_be_login_form(self):
@@ -25,5 +23,3 @@ class MainPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present (*LoginPageLocators.REGISTER_FORM) , "No register form on this page"
-#def should_be_login_link(self):
-#    assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
